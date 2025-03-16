2975f502028367bf08c4849a3cb4a259fb41d67f
P Chaitanya Sayee <chaitanyapsayee@gmail.com>
2025-03-16 09:39:37 -0700

    Day-05

---
+++ Day-05/Subsets/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans, sol = [], []
+
+        def backtrack(i):
+            if i == n:
+                ans.append(sol[:])
+                return
+
+            # Don't pick nums[i]
+            backtrack(i + 1)
+
+            # Pick nums[i]
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
+
+        backtrack(0)
+        return ans
