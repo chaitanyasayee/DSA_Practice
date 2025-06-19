575ce1fecc0b0b76a2ee83de5395cf8f5b6b1972
P Chaitanya Sayee <chaitanyapsayee@gmail.com>
2025-06-19 19:52:09 +0530

    day 100

---
+++ Day-100/Partition Array Such That Maximum Difference Is K/sol.py
@@ -0,0 +1,10 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 1
+        start = nums[0]
+        for num in nums:
+            if num - start > k:
+                ans +=1
+                start = num
+        return ans
